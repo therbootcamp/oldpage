af7f0b4330b79dce263796d3dd822d2da5a9495a
Dirk Wulff <dirk.wulff@gmail.com>
2017-09-29 16:13:42 +0200

    stuff

---
+++ _materials/knitMe/knitrHack.R
@@ -0,0 +1,26 @@
+
+# In order to knit use this command rather than the knit button
+# Make sure to replace the path with the one leading to the specific markdown (full path)
+# Then access files using ../folder/file
+# Also change the css line (line 8) to:      css: ["default", "../_css/my-theme.css"]
+
+# Dirk's version
+xaringan::inf_mr('~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions/D1S0_Intro/Intro.Rmd',cast_from = '~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions')
+xaringan::inf_mr('~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions/D1S0_Intro/Intro.Rmd',cast_from = '~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions')
+xaringan::inf_mr('~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions/D1S0_Intro/Intro.Rmd',cast_from = '~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions')
+xaringan::inf_mr('~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions/D1S0_Intro/Intro.Rmd',cast_from = '~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions')
+xaringan::inf_mr('~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions/D1S0_Intro/Intro.Rmd',cast_from = '~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions')
+xaringan::inf_mr('~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions/D1S0_Intro/Intro.Rmd',cast_from = '~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions')
+xaringan::inf_mr('~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions/D1S0_Intro/Intro.Rmd',cast_from = '~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions')
+xaringan::inf_mr('~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions/D1S0_Intro/Intro.Rmd',cast_from = '~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions')
+
+
+# Tanny's version
+xaringan::inf_mr('~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions/D1S0_Intro/Intro.Rmd',cast_from = '~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions')
+xaringan::inf_mr('~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions/D1S0_Intro/Intro.Rmd',cast_from = '~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions')
+xaringan::inf_mr('~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions/D1S0_Intro/Intro.Rmd',cast_from = '~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions')
+xaringan::inf_mr('~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions/D1S0_Intro/Intro.Rmd',cast_from = '~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions')
+xaringan::inf_mr('~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions/D1S0_Intro/Intro.Rmd',cast_from = '~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions')
+xaringan::inf_mr('~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions/D1S0_Intro/Intro.Rmd',cast_from = '~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions')
+xaringan::inf_mr('~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions/D1S0_Intro/Intro.Rmd',cast_from = '~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions')
+
