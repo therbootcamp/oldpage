9dbcf39cd089382855c745cf5cd6492133cf3319
Nathaniel Phillips <Nathaniel.D.Phillips.is@gmail.com>
2017-10-03 16:04:43 +0200

    updated all Nathaniel's sessions to use new css file

--- _materials/knitMe/knitrHack.R
+++ _materials/knitMe/knitrHack.R
@@ -4,6 +4,10 @@
 # Then access files using ../folder/file
 # Also change the css line (line 8) to:      css: ["default", "../_css/my-theme.css"]
 
+global_path <- "~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions"   # Dirk
+global_path <- "~/Dropbox/therbootcamp/therbootcamp.github.io/_sessions/"   # Tanny
+
+
 # Dirk's version
 xaringan::inf_mr('~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions/D1S0_Intro/Intro.Rmd',cast_from = '~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions')
 xaringan::inf_mr('~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions/D1S0_Intro/Intro.Rmd',cast_from = '~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions')
@@ -16,7 +20,13 @@ xaringan::inf_mr('~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions/D1S0_
 
 
 # Tanny's version
-xaringan::inf_mr('~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions/D1S0_Intro/Intro.Rmd',cast_from = '~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions')
+xaringan::inf_mr(paste0(global_path, "/D2S1_Wrangling/Wrangling.Rmd"), cast_from = global_path)
+xaringan::inf_mr(paste0(global_path, "/D2S2_Statistics/Statistics.Rmd"), cast_from = global_path)
+xaringan::inf_mr(paste0(global_path, "/D2S3_MachineLearning/MachineLearning.Rmd"), cast_from = global_path)
+xaringan::inf_mr(paste0(global_path, "/D3S2_PlottingI/PlottingI.Rmd"), cast_from = global_path)
+xaringan::inf_mr(paste0(global_path, "/D3S3_PlottingII/PlottingII.Rmd"), cast_from = global_path)
+xaringan::inf_mr(paste0(global_path, "/D4S2_DynamicReports/DynamicReports.Rmd"), cast_from = global_path)
+
 xaringan::inf_mr('~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions/D1S0_Intro/Intro.Rmd',cast_from = '~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions')
 xaringan::inf_mr('~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions/D1S0_Intro/Intro.Rmd',cast_from = '~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions')
 xaringan::inf_mr('~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions/D1S0_Intro/Intro.Rmd',cast_from = '~/Dropbox (2.0)/Work/Software/webpage_bootcamp/_sessions')
