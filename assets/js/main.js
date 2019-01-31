d5ef41fbe8413d7db963a8afec762e06f1ec0670
Dirk Wulff <dirk.wulff@gmail.com>
2019-01-31 09:04:34 +0100

    dw

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded",function(){
   new SweetScroll({});
   particlesJS("particles-js",
     {particles:{
-      number:{value:10,density:{enable:!0,value_area:800}},
+      number:{value:100,density:{enable:!0,value_area:800}},
       color:{value:"#EACC48"},
       shape:{type:"polygon",stroke:{width:0,color:"#000000"},polygon:{nb_sides:5},image:{src:"img/github.svg",width:100,height:100}},
       opacity:{value:.5,random:!1,anim:{enable:!1,speed:1,opacity_min:.1,sync:!1}},
